5fffc44647ecd869c1bc355661be59e1d8fb3bdb
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-07-11 15:25:24 +0700

    add delete commands and add aliases

--- packages/cli/src/modules/key/entrypoint.rs
+++ packages/cli/src/modules/key/entrypoint.rs
@@ -1,19 +1,23 @@
 use super::config::KeyConfig;
 use crate::framework::{Context, Module};
 use crate::support::signer::SigningKeyExt;
-use anyhow::{Context as _, Result};
+use anyhow::{Context as _, Ok, Result};
 use clap::Subcommand;
+use cosmrs::bip32;
+use cosmrs::bip32::secp256k1::elliptic_curve::rand_core::OsRng;
 use cosmrs::crypto::secp256k1::SigningKey;
 
 // TODO:
 // -- DONE
 // beaker key set username "camel hours ..."
 // beaker key address <username>
+// beaker key gen username [--show]
+// beaker key delete username
 //
 // -- TODO
-// beaker key list
-// beaker key gen username
-// beaker key delete username
+// confirm on overrride
+// prevent wrong mnemonic to not pass
+// add alias
 
 #[derive(Subcommand, Debug)]
 pub enum KeyCmd {
@@ -24,10 +28,27 @@ pub enum KeyCmd {
         /// Mnemonic string to store as an entry
         mnemonic: String,
     },
+    /// Delete existing key
+    #[clap(alias = "del")]
+    Delete {
+        /// Name of the key to create or update
+        name: String,
+    },
     /// Get address from keyring's stored key
+    #[clap(alias = "addr")]
     Address {
         /// Name of the key to create or update
         name: String,
+    },
+    /// Generate new mnemonic
+    #[clap(alias = "gen")]
+    Generate {
+        /// Name of the key to create or update
+        name: String,
+
+        /// Show mnemonic in the console if set, keep it secret otherwise
+        #[clap(long)]
+        show: bool,
     },
 }
 
@@ -41,6 +62,12 @@ impl<'a> Module<'a, KeyConfig, KeyCmd, anyhow::Error> for KeyModule {
                 entry
                     .set_password(mnemonic)
                     .with_context(|| "Unable to set key")
+            }
+            KeyCmd::Delete { name } => {
+                let entry = keyring::Entry::new(&ctx.config()?.service, name);
+                entry
+                    .delete_password()
+                    .with_context(|| "Unable to delete key")
             }
             KeyCmd::Address { name } => {
                 let entry = keyring::Entry::new(&ctx.config()?.service, name);
@@ -56,6 +83,20 @@ impl<'a> Module<'a, KeyConfig, KeyCmd, anyhow::Error> for KeyModule {
 
                 println!("{}", address);
                 Ok(())
+            }
+            KeyCmd::Generate { name, show } => {
+                let mnemonic = bip32::Mnemonic::random(OsRng, bip32::Language::English);
+                let mnemonic = mnemonic.phrase();
+
+                let entry = keyring::Entry::new(&ctx.config()?.service, name);
+
+                if *show {
+                    println!("{}", mnemonic);
+                }
+
+                entry
+                    .set_password(mnemonic)
+                    .with_context(|| "Unable to set key")
             }
         }
     }
