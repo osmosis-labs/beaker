75e11a4943af7ecc4169c1e5f800f5aa6978855c
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-09-22 15:34:43 +0700

    fix failing doc build

--- packages/cli/src/lib.rs
+++ packages/cli/src/lib.rs
@@ -1,5 +1,27 @@
-#![allow(rustdoc::invalid_rust_codeblocks)]
-#![doc = include_str!("../../../README.md") ]
+#![doc = r#"
+# Beaker
+
+<p align="center">
+<a href="https://docs.osmosis.zone/developing/dapps/get_started/">
+    <img src="https://github.com/osmosis-labs/beaker/blob/main/assets/beaker.png?raw=true" alt="Beaker logo" title="Beaker" align="center" height="150" />
+</a>
+</p>
+
+<p align="center" width="100%">
+    <img  height="20" src="https://github.com/osmosis-labs/beaker/actions/workflows/doctest.yml/badge.svg">
+    <img height="20" src="https://github.com/osmosis-labs/beaker/actions/workflows/lint.yml/badge.svg">
+    <a href="https://github.com/osmosis-labs/beaker/blob/main/LICENSE-APACHE"><img height="20" src="https://img.shields.io/badge/license-APACHE-blue.svg"></a>
+    <a href="https://github.com/osmosis-labs/beaker/blob/main/LICENSE-MIT"><img height="20" src="https://img.shields.io/badge/license-MIT-blue.svg"></a>
+    <a href="https://deps.rs/repo/github/osmosis-labs/beaker"><img height="20" src="https://deps.rs/repo/github/osmosis-labs/beaker/status.svg"></a>
+    <a href="https://crates.io/crates/beaker"><img height="20" src="https://img.shields.io/crates/v/beaker.svg"></a>
+</p>
+
+[Beaker](https://github.com/osmosis-labs/beaker) makes it easy to scaffold a new cosmwasm app, with all of the dependencies for osmosis hooked up, interactive console, a sample front-end, client generation and more.
+
+- [Getting Started](https://github.com/osmosis-labs/beaker)
+- [Command Reference](https://github.com/osmosis-labs/beaker/tree/main/docs/commands)
+- [Config Reference](https://github.com/osmosis-labs/beaker/tree/main/docs/config)
+"#]
 
 mod framework;
 mod modules;
