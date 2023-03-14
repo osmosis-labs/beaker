7acefba28a89059de4e73254b2c38ed353291676
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2023-03-14 16:16:22 +0700

    allow matching cli args def from script

--- packages/cli/src/modules/task/entrypoint.rs
+++ packages/cli/src/modules/task/entrypoint.rs
@@ -1,8 +1,12 @@
 use super::{config::TaskConfig, script_mod::wasm};
 use crate::framework::{Context, Module};
 use anyhow::Result;
-use clap::Subcommand;
-use rhai::{exported_module, serde::to_dynamic, Dynamic, Engine, Scope};
+use clap::{Arg, Command, Subcommand};
+use rhai::{
+    exported_module,
+    serde::{from_dynamic, to_dynamic},
+    Dynamic, Engine, EvalAltResult, Map,
+};
 
 #[derive(Subcommand, Debug)]
 #[clap(trailing_var_arg = true)]
@@ -34,27 +38,45 @@ impl<'a> Module<'a, TaskConfig, TaskCmd, anyhow::Error> for TaskModule {
                     .join(config.tasks_path)
                     .join(format!("{}.rhai", script));
 
-                let mut scope = Scope::new();
+                let moved_script = script.to_owned();
 
-                // TODO: using [clap-serde](https://github.com/aobatact/clap-serde#json) to
-                // allow defining expected args from .rhai file
-                scope.push(
-                    "args",
-                    args.iter()
-                        .map(|s| to_dynamic(s).unwrap())
-                        .collect::<Vec<Dynamic>>(),
+                let args = args.to_owned();
+
+                engine.register_fn(
+                    "match_args",
+                    move |arg_defs: Dynamic| -> Result<Map, Box<EvalAltResult>> {
+                        let arg_defs = from_dynamic::<Vec<&str>>(&arg_defs)?;
+
+                        let prog_name = format!("beaker task run {}", moved_script);
+
+                        let matches = Command::new(prog_name.clone())
+                            .args(arg_defs.clone().into_iter().map(|arg| {
+                                Arg::new(arg).long(arg).required(true).takes_value(true)
+                            }))
+                            .try_get_matches_from(
+                                // emulate calling cli command with args
+                                vec![prog_name].iter().chain(args.to_owned().iter()),
+                            )
+                            .map_err(|e| <Box<EvalAltResult>>::from(e.to_string()))?;
+
+                        arg_defs
+                            .into_iter()
+                            .map(|arg| -> Result<_, Box<EvalAltResult>> {
+                                let matched = matches.value_of(arg).unwrap_or("");
+                                Ok((arg.into(), to_dynamic(matched)?))
+                            })
+                            .collect::<Result<Map, _>>()
+                    },
                 );
 
-                engine
-                    .run_file_with_scope(&mut scope, script_path.clone())
-                    .map_err(|e| {
-                        anyhow::anyhow!(
-                            "Failed to run script `{}` ({}):\n{}",
-                            script,
-                            script_path.display(),
-                            e.to_string()
-                        )
-                    })?;
+                engine.run_file(script_path.clone()).map_err(|e| {
+                    anyhow::anyhow!(
+                        "Failed to run script `{}` ({}):\n{}",
+                        script,
+                        script_path.display(),
+                        e.to_string()
+                    )
+                })?;
                 Ok(())
             }
         }
